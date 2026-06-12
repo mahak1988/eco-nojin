d40188187a4d0807610315e86db9d02f1e52bd3b
narvan2050 <ai-assistant@econojin.com>
2026-06-12 05:03:45 +0330

    fix(phase3b): move analysis/index.ts and remove nested folder

--- packages/features/src/analysis/analysis/index.ts
+++
@@ -1,5 +0,0 @@
-export { AnalysisDashboard } from './AnalysisDashboard';
-export { AnalysisForm } from './AnalysisForm';
-export { ChartsPanel } from './ChartsPanel';
-export { MapPanel } from './MapPanel';
-export { EventsLog } from './EventsLog';
