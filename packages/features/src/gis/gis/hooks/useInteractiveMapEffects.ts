40cc29d7fc2429ee6c1fdbe4af4708ad02205e7e
narvan2050 <ai-assistant@econojin.com>
2026-07-15 08:59:26 +0330

    Revert "refactor(naming): rename hydrology-frontend to @econojin/web"
    This reverts commit 60dfd7762bea6311ee18aab6ee289a259e7a6cb9.

--- packages/features/src/gis/gis/hooks/useInteractiveMapEffects.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-export function useInteractiveMapEffects() {
-  // TODO: منتقل کردن useEffect ها از InteractiveMap
-
-  useEffect(() => {
-    // TODO
-  }, [])
-}
