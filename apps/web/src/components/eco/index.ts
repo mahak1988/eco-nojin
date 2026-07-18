5285cd49716ab10748d8b15ebb7a13ab510c9a06
mahak1988 <princemahak00@gmail.com>
2026-07-18 08:05:45 +0330

    feat(design-system): barrel exports for eco design components

---
+++ apps/web/src/components/eco/index.ts
@@ -0,0 +1,11 @@
+export { ModuleCard } from "./ModuleCard";
+export type { ModuleCardProps, ModuleGradient } from "./ModuleCard";
+
+export { HeroSection } from "./HeroSection";
+export type { HeroSectionProps } from "./HeroSection";
+
+export { EcoButton } from "./EcoButton";
+export type { EcoButtonProps } from "./EcoButton";
+
+export { DashboardGrid } from "./DashboardGrid";
+export type { DashboardGridProps } from "./DashboardGrid";
