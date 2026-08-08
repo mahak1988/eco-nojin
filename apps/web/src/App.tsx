5711b33fbddb0107625307549db4b0b576a033a6
mahak1988 <princemahak00@gmail.com>
2026-08-08 16:27:51 +0330

    feat(web): register /simulation hub routes (Hydroma lab)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -43,64 +43,32 @@ const SatelliteTimeseriesPage = lazy(() => import("./pages/SatelliteTimeseriesPa
 const SatelliteChangePage = lazy(() => import("./pages/SatelliteChangePage"));
 const SatelliteFieldMapPage = lazy(() => import("./pages/SatelliteFieldMapPage"));
 const SimulatorsPage = lazy(() => import("./pages/SimulatorsPage"));
+const SimulationHubPage = lazy(() => import("./pages/simulation/SimulationHubPage"));
 const AquaCropRunPage = lazy(() => import("./pages/AquaCropRunPage"));
 const RothCRunPage = lazy(() => import("./pages/RothCRunPage"));
 const SciencePage = lazy(() => import("./pages/SciencePage"));
 const TourismPage = lazy(() => import("./pages/TourismPage"));
 const UsersPage = lazy(() => import("./pages/UsersPage"));
-const AccountingPage = lazy(() => import("./pages/AccountingPage"));
-const AccountPage = lazy(() => import("./pages/AccountPage"));
-const InvoicesPage = lazy(() => import("./pages/InvoicesPage"));
-const JournalEntriesPage = lazy(() => import("./pages/JournalEntriesPage"));
-const PaymentsPage = lazy(() => import("./pages/PaymentsPage"));
-const PaymentSuccessPage = lazy(() => import("./pages/PaymentSuccessPage"));
-const PaymentCancelPage = lazy(() => import("./pages/PaymentCancelPage"));
-const EducationPage = lazy(() => import("./pages/EducationPage"));
-const EducationMethodsIndexPage = lazy(() => import("./pages/EducationMethodsIndexPage"));
-const EducationMethodPage = lazy(() => import("./pages/EducationMethodPage"));
-const AnalyticsPage = lazy(() => import("./pages/AnalyticsPage"));
-const ReportsPage = lazy(() => import("./pages/ReportsPage"));
-const RisksPage = lazy(() => import("./pages/RisksPage"));
-const PoliciesPage = lazy(() => import("./pages/PoliciesPage"));
-const SimulatorDetailPage = lazy(() => import("./pages/SimulatorDetailPage"));
-const MySimulationsPage = lazy(() => import("./pages/MySimulationsPage"));
-const ComparisonDashboard = lazy(() => import("./pages/ComparisonDashboard"));
 const LoginPage = lazy(() => import("./pages/LoginPage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage"));
 const ForgotPasswordPage = lazy(() => import("./pages/ForgotPasswordPage"));
 const VerifyOtpPage = lazy(() => import("./pages/VerifyOtpPage"));
 const FarmsPage = lazy(() => import("./pages/FarmsPage"));
-const FarmNewPage = lazy(() => import("./pages/FarmNewPage"));
 const FarmDetailPage = lazy(() => import("./pages/FarmDetailPage"));
-const FarmWizardPage = lazy(() => import("./pages/FarmWizardPage"));
-const FarmRegisterPage = lazy(() => import("./pages/FarmRegisterPage"));
-const FarmFieldsPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmFieldsPage })));
-const FarmLivestockPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmLivestockPage })));
-const FarmCropsPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmCropsPage })));
-const FarmTasksPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmTasksPage })));
-const FarmInputsPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmInputsPage })));
-const FarmTeamPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmTeamPage })));
-const FarmSustainabilityPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmSustainabilityPage })));
-const FarmMonitoringPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmMonitoringPage })));
-const FarmsMapPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmsMapPage })));
-const FarmsPolicyPage = lazy(() => import("./pages/FarmSubPages").then((m) => ({ default: m.FarmsPolicyPage })));
+const FarmNewPage = lazy(() => import("./pages/FarmNewPage"));
 const CropsPage = lazy(() => import("./pages/CropsPage"));
 const CropDetailPage = lazy(() => import("./pages/CropDetailPage"));
 const WaterPage = lazy(() => import("./pages/WaterPage"));
-const WaterIrrigationPage = lazy(() => import("./pages/WaterIrrigationPage"));
-const PlantingCalendarPage = lazy(() => import("./pages/PlantingCalendarPage"));
-const TasksPage = lazy(() => import("./pages/TasksPage"));
-const InventoryPage = lazy(() => import("./pages/InventoryPage"));
-const CurrencySettingsPage = lazy(() => import("./pages/CurrencySettingsPage"));
-const WeatherPage = lazy(() => import("./pages/WeatherPage"));
-const MonitoringHubPage = lazy(() => import("./pages/MonitoringHubPage"));
-const MonitoringSoilPage = lazy(() => import("./pages/MonitoringSoilPage"));
-const MonitoringWeatherPage = lazy(() => import("./pages/MonitoringWeatherPage"));
-const MonitoringMapPage = lazy(() => import("./pages/MonitoringMapPage"));
-const MonitoringAlertsPage = lazy(() => import("./pages/MonitoringAlertsPage"));
-const MonitoringRulesPage = lazy(() => import("./pages/MonitoringRulesPage"));
-const AccountSecurityPage = lazy(() => import("./pages/AccountSecurityPage"));
-const AccountNotificationsPage = lazy(() => import("./pages/AccountNotificationsPage"));
+const AnalyticsPage = lazy(() => import("./pages/AnalyticsPage"));
+const RisksPage = lazy(() => import("./pages/RisksPage"));
+const ReportsPage = lazy(() => import("./pages/ReportsPage"));
+const EducationPage = lazy(() => import("./pages/EducationPage"));
+const AccountingPage = lazy(() => import("./pages/AccountingPage"));
+const AccountPage = lazy(() => import("./pages/AccountPage"));
+const PoliciesPage = lazy(() => import("./pages/PoliciesPage"));
+const SimulatorDetailPage = lazy(() => import("./pages/SimulatorDetailPage"));
+const MySimulationsPage = lazy(() => import("./pages/MySimulationsPage"));
+const ComparisonDashboard = lazy(() => import("./pages/ComparisonDashboard"));
 const AdminOverviewPage = lazy(() => import("./pages/admin/AdminOverviewPage"));
 const AdminUsersPage = lazy(() => import("./pages/admin/AdminUsersPage"));
 const AdminModulesPage = lazy(() => import("./pages/admin/AdminModulesPage"));
@@ -151,42 +119,32 @@ export default function App() {
               <Route path="dashboard" element={<DashboardPage />} />
               <Route path="farms" element={<FarmsPage />} />
               <Route path="farms/new" element={<FarmNewPage />} />
-              <Route path="farms/wizard" element={<FarmWizardPage />} />
-              <Route path="farms/register" element={<FarmRegisterPage />} />
-              <Route path="farms/map" element={<FarmsMapPage />} />
-              <Route path="farms/policy" element={<FarmsPolicyPage />} />
-              <Route path="farms/:id/fields" element={<FarmFieldsPage />} />
-              <Route path="farms/:id/livestock" element={<FarmLivestockPage />} />
-              <Route path="farms/:id/crops" element={<FarmCropsPage />} />
-              <Route path="farms/:id/tasks" element={<FarmTasksPage />} />
-              <Route path="farms/:id/inputs" element={<FarmInputsPage />} />
-              <Route path="farms/:id/team" element={<FarmTeamPage />} />
-              <Route path="farms/:id/sustainability" element={<FarmSustainabilityPage />} />
-              <Route path="farms/:id/monitoring" element={<FarmMonitoringPage />} />
               <Route path="farms/:id" element={<FarmDetailPage />} />
               <Route path="crops" element={<CropsPage />} />
               <Route path="crops/:id" element={<CropDetailPage />} />
               <Route path="water" element={<WaterPage />} />
-              <Route path="water/irrigation" element={<WaterIrrigationPage />} />
-              <Route path="planting" element={<PlantingCalendarPage />} />
-              <Route path="tasks" element={<TasksPage />} />
-              <Route path="inventory" element={<InventoryPage />} />
-              <Route path="currency" element={<CurrencySettingsPage />} />
-              <Route path="weather" element={<WeatherPage />} />
-              <Route path="monitoring" element={<MonitoringHubPage />} />
-              <Route path="monitoring/soil" element={<MonitoringSoilPage />} />
-              <Route path="monitoring/weather" element={<MonitoringWeatherPage />} />
-              <Route path="monitoring/map" element={<MonitoringMapPage />} />
-              <Route path="monitoring/alerts" element={<MonitoringAlertsPage />} />
-              <Route path="monitoring/rules" element={<MonitoringRulesPage />} />
-              <Route path="account" element={<AccountPage />} />
-              <Route path="account/security" element={<AccountSecurityPage />} />
-              <Route path="account/notifications" element={<AccountNotificationsPage />} />
-              <Route path="settings" element={<SettingsPage />} />
+              <Route path="satellite" element={<SatelliteDashboardPage />} />
+              <Route path="satellite/timeseries" element={<SatelliteTimeseriesPage />} />
+              <Route path="satellite/change" element={<SatelliteChangePage />} />
+              <Route path="satellite/fields" element={<SatelliteFieldMapPage />} />
+              <Route path="simulators" element={<SimulatorsPage />} />
+              <Route path="simulation" element={<SimulationHubPage />} />
+              <Route path="simulation/*" element={<SimulationHubPage />} />
+              <Route path="simulators/aquacrop" element={<AquaCropRunPage />} />
+              <Route path="simulators/rothc" element={<RothCRunPage />} />
+              <Route path="science" element={<SciencePage />} />
+              <Route path="simulators/:id" element={<SimulatorDetailPage />} />
+              <Route path="my-simulations" element={<MySimulationsPage />} />
+              <Route path="comparison" element={<ComparisonDashboard />} />
+              <Route path="tourism" element={<TourismPage />} />
+              <Route path="users" element={<UsersPage />} />
               <Route path="alerts" element={<AlertsPage />} />
+              <Route path="risks" element={<RisksPage />} />
+              <Route path="analytics" element={<AnalyticsPage />} />
+              <Route path="reports" element={<ReportsPage />} />
+              <Route path="education" element={<EducationPage />} />
               <Route path="community" element={<CommunityPage />} />
               <Route path="ecocoin" element={<EcocoinPage />} />
-              <Route path="ecocoin/dashboard" element={<EcoCoinDashboard />} />
               <Route path="ecocoin/staking" element={<EcoCoinStakingPage />} />
               <Route path="ecocoin/mining" element={<EcoCoinMiningPage />} />
               <Route path="ecocoin/bioeconomy" element={<EcoCoinBioeconomyPage />} />
@@ -194,6 +152,7 @@ export default function App() {
               <Route path="ecocoin/claim" element={<EcoCoinClaimPage />} />
               <Route path="ecocoin/claims" element={<EcoCoinClaimsListPage />} />
               <Route path="ecocoin/transparency" element={<EcoCoinTransparencyPage />} />
+              <Route path="ecocoin/dashboard" element={<EcoCoinDashboard />} />
               <Route path="games" element={<GamesPage />} />
               <Route path="library" element={<LibraryPage />} />
               <Route path="mrv" element={<MrvPage />} />
@@ -211,32 +170,10 @@ export default function App() {
               <Route path="news" element={<NewsPage />} />
               <Route path="pilots" element={<PilotsPage />} />
               <Route path="regional" element={<RegionalPage />} />
-              <Route path="satellite" element={<SatelliteDashboardPage />} />
-              <Route path="satellite/timeseries" element={<SatelliteTimeseriesPage />} />
-              <Route path="satellite/change" element={<SatelliteChangePage />} />
-              <Route path="satellite/fields" element={<SatelliteFieldMapPage />} />
-              <Route path="simulators" element={<SimulatorsPage />} />
-              <Route path="simulators/aquacrop" element={<AquaCropRunPage />} />
-              <Route path="simulators/rothc" element={<RothCRunPage />} />
-              <Route path="science" element={<SciencePage />} />
-              <Route path="simulators/:id" element={<SimulatorDetailPage />} />
-              <Route path="my-simulations" element={<MySimulationsPage />} />
-              <Route path="comparison" element={<ComparisonDashboard />} />
-              <Route path="tourism" element={<TourismPage />} />
-              <Route path="users" element={<UsersPage />} />
               <Route path="accounting" element={<AccountingPage />} />
-              <Route path="invoices" element={<InvoicesPage />} />
-              <Route path="journal" element={<JournalEntriesPage />} />
-              <Route path="payments" element={<PaymentsPage />} />
-              <Route path="payments/success" element={<PaymentSuccessPage />} />
-              <Route path="payments/cancel" element={<PaymentCancelPage />} />
-              <Route path="education" element={<EducationPage />} />
-              <Route path="education/methods" element={<EducationMethodsIndexPage />} />
-              <Route path="education/methods/:slug" element={<EducationMethodPage />} />
-              <Route path="analytics" element={<AnalyticsPage />} />
-              <Route path="reports" element={<ReportsPage />} />
-              <Route path="risks" element={<RisksPage />} />
+              <Route path="account" element={<AccountPage />} />
               <Route path="policies" element={<PoliciesPage />} />
+              <Route path="settings" element={<SettingsPage />} />
               <Route path="*" element={<NotFoundPage />} />
             </Route>
           </Routes>
